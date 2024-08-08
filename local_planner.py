3bd659c2e4cb4fa384b37a2a69ec3a955f697a0f
ynahum <yair.y.nahum@intel.com>
2024-08-08 11:57:31 +0300

    rm main code from local_planner

--- local_planner.py
+++ local_planner.py
@@ -1,9 +1,8 @@
 import numpy as np
 import math
 from trajectory import Trajectory
-import car_consts
 import matplotlib.pyplot as plt
-from car_simulator import State, States, Simulator
+from car_simulator import State, States
 
 
 
@@ -110,44 +109,3 @@ def plot_error(closest_path_coords, states:States, trajectory:Trajectory):
     ax.set_ylabel('Tracking Error')
     ax.grid()
     plt.show()
-
-def main():
-    #  hyper-parameters
-    k = 0.1  # look forward gain
-    Lfc = 1.0  # [m] look-ahead distance
-    Kp = 1.0  # speed proportional gain
-    dt = 0.1  # [s] time tick
-    target_speed = 1.0  # [m/s]
-    T = 100.0  # max simulation time
-    WB = car_consts.wheelbase 
-    MAX_STEER = car_consts.max_steering_angle_rad  # maximum steering angle [rad]
-    MAX_DSTEER = car_consts.max_dt_steering_angle  # maximum steering speed [rad/s]
-    MAX_SPEED = car_consts.max_linear_velocity  # maximum speed [m/s]
-    MIN_SPEED = car_consts.min_linear_velocity  # minimum speed [m/s]
-    MAX_ACCEL = 1.0  # maximum accel [m/ss]
-
-    path = np.load('path_maze_meter_sim.npy')
-    trajectory = Trajectory(dl=0.1, path =path, TARGET_SPEED=target_speed)
-    state = State(x=trajectory.cx[0], y=trajectory.cy[0], yaw=trajectory.cyaw[0], v=0.0)
-    lastIndex = len(trajectory.cx) - 1
-    clock = 0.0
-    states = States()
-    states.append(clock, state)
-    pp = PurePursuit_Controller(trajectory.cx, trajectory.cy, k, Lfc, Kp, WB, MAX_ACCEL, MAX_SPEED, MIN_SPEED, MAX_STEER, MAX_DSTEER)
-    target_ind, _, nearest_index = pp.search_target_index(state)
-    simulator = Simulator(trajectory, dt)
-    closest_path_coords = []
-    while T >= clock and lastIndex > target_ind:
-        state.v = pp.proportional_control_acceleration(target_speed, state.v, dt)
-        delta, target_ind, closest_index = pp.pure_pursuit_steer_control(state, trajectory, dt)
-        state.predelta = delta
-        state = simulator.update_state(state, delta)  # Control vehicle
-        clock += dt
-        states.append(clock, state, delta)
-        closest_path_coords.append([trajectory.cx[closest_index], trajectory.cy[closest_index]])
-    simulator.show_simulation(states, closest_path_coords)
-    plot_error(closest_path_coords, states, trajectory)
-
-
-if __name__ == '__main__':
-    main()
