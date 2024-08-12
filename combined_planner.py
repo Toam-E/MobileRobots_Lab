78859ea31cd0eda26baebb90a1e170c6b7ee80ae
ynahum <yair.y.nahum@intel.com>
2024-08-12 14:18:13 +0300

    rename local planner back to pure pursuit controller

--- combined_planner.py
+++ combined_planner.py
@@ -3,21 +3,20 @@ from trajectory import Trajectory
 from consts import *
 from kino_rrt import KINORRT
 from car_simulator import SimState, SimStatesContainer, Simulator
-from local_planner import LocalPlanner
+from pure_pursuit import PurePursuitController
 from cspace import CSpace
 
-# run params
-RUN_KRRT = False
-RUN_ADD_OBS = True
-RUN_COMBINED_PLANNER = True
-RUN_ANIMATION = True
+KRRT_DEPTH = 3
+MPC_KRRT_TOTAL_ITER = 100
+MPC_NUM_OF_PATHS_TO_CMP = 3
+MPC_ITER = 3
 
 class CombinedPlanner(object):
 
     def __init__(self, obs_map, trajectory : Trajectory, converter: CSpace,\
                   sim : Simulator, states : SimStatesContainer):
 
-        self.lp = LocalPlanner(converter, obs_map, trajectory.cx, trajectory.cy,\
+        self.lp = PurePursuitController(converter, obs_map, trajectory.cx, trajectory.cy,\
                             LF_K, LFC, V_KP, WB, MAX_ACCEL, MAX_SPEED,\
                             MIN_SPEED, MAX_STEER, MAX_DSTEER)
         self.trajectory = trajectory
@@ -27,6 +26,8 @@ class CombinedPlanner(object):
         self.clock = 0.0
         self.target_path_coords = []
         self.closest_path_coords = []
+        self.mpc_mode = False
+        self.mpc_curr_iteration = 0
 
     def find_path(self):
         cur_state = SimState(x=self.trajectory.cx[0], y=self.trajectory.cy[0], yaw=self.trajectory.cyaw[0], v=0.0)
@@ -38,12 +39,20 @@ class CombinedPlanner(object):
 
         while T >= self.clock and self.lastIndex > target_ind:
             cur_state = copy.copy(cur_state)
+            
             if self.lp.local_obs_detected(cur_state, cone_radius=SENSE_CONE_RADIUS, cone_fov=SENSE_CONE_ANGLE):
-                # run KRRT locally and find a new route
-                pass
+                self.mpc_mode = True
+                self.mpc_curr_iteration = 0
 
             cur_state.v = self.lp.proportional_control_acceleration(TARGETED_SPEED, cur_state.v, DELTA_T)
-            delta, target_ind, closest_index = self.lp.pure_pursuit_steer_control(cur_state, self.trajectory, DELTA_T)
+
+            if self.mpc_mode:
+                delta, target_ind, closest_index = self.lp.pure_pursuit_steer_control(cur_state, self.trajectory, DELTA_T)
+                #delta, target_ind, closest_index = self.mpc_steer_control(cur_state, DELTA_T)
+                
+            else:
+                delta, target_ind, closest_index = self.lp.pure_pursuit_steer_control(cur_state, self.trajectory, DELTA_T)
+
             cur_state.predelta = delta
             cur_state = self.sim.update_state(cur_state, delta)  # Control vehicle
             self.clock += DELTA_T
@@ -53,3 +62,6 @@ class CombinedPlanner(object):
             self.closest_path_coords.append([self.trajectory.cx[closest_index], self.trajectory.cy[closest_index]])
             self.target_path_coords.append([self.trajectory.cx[target_ind], self.trajectory.cy[target_ind]])
 
+    def mpc_steer_control(self, state: SimState, dt):
+        #return delta, target_ind, closest_index
+        pass
