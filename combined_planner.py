fix delta calculation based on next state (one step in MPC)
also, added:
1. speed control based on theta dot (tune a bit)
2. tune cone angle and radius
3. added debug enable/disable prints

combined_planner.py
@@ -5,6 +5,8 @@ from car_simulator import SimState, SimStatesContainer, Simulator
 from pure_pursuit import PurePursuitController
 from model_predictive_controller import ModelPredictiveController
 from cspace import CSpace
+from utils import get_normalized_angle
+from geo_utils import *
 
 class CombinedPlanner(object):
 
@@ -15,6 +17,8 @@ class CombinedPlanner(object):
                             LF_K, LFC, V_KP, WB, MAX_ACCEL, MAX_SPEED,\
                             MIN_SPEED, MAX_STEER, MAX_DSTEER)
         self.mpc = ModelPredictiveController(obs_map, trajectory, converter)
+        self.obs_map = obs_map
+        self.converter = converter
         self.trajectory = trajectory
         self.sim = sim
         self.states = states
@@ -24,6 +28,29 @@ class CombinedPlanner(object):
         self.closest_path_coords = []
         self.mpc_mode = False
         self.mpc_curr_iteration = 0
+
+    def local_obs_detected(self, state: SimState, cone_radius = 15, cone_fov=np.pi/3):
+        state_meter_list = [state.x, state.y, state.yaw]
+        state_pixel = self.converter.meter2pixel(state_meter_list)
+        cone_origin_x = state_pixel[0]
+        cone_origin_y = state_pixel[1]
+        cone_origin_yaw = state_pixel[2]
+        fov_angles = np.linspace(start=cone_fov/2, stop=-cone_fov/2, num=cone_radius)
+        tile_yaw = np.tile(cone_origin_yaw, fov_angles.size)
+        fov_angles = np.expand_dims(tile_yaw + fov_angles, axis=0)
+        car_angles = np.apply_along_axis(get_normalized_angle, 0, fov_angles)
+        car_cone_xs = cone_origin_x + cone_radius * np.cos(car_angles)
+        car_cone_ys = cone_origin_y + cone_radius * np.sin(car_angles)
+        car_cone_xs = np.append(np.insert(car_cone_xs, 0, cone_origin_x), cone_origin_x)
+        car_cone_ys = np.append(np.insert(car_cone_ys, 0, cone_origin_y), cone_origin_y)
+        # check if cone is in collision
+        cone_points = [list(car_cone_point) for car_cone_point in zip(car_cone_xs.flatten(), car_cone_ys.flatten())]
+        min_x, min_y, max_x, max_y = polygon_calculate_bounding_box(cone_points)
+        min_x, min_y, max_x, max_y = clip_bounding_box(min_x, min_y, max_x, max_y, self.obs_map.shape)
+        roi = extract_roi(self.obs_map, min_x, min_y, max_x, max_y)
+        adjusted_polygon = adjust_polygon_to_roi(cone_points, min_x, min_y)
+        state.obs_ahead = check_polygon_intersection(roi, adjusted_polygon)
+        return state.obs_ahead
 
     def find_path(self):
         cur_state = SimState(x=self.trajectory.cx[0], y=self.trajectory.cy[0], yaw=self.trajectory.cyaw[0], v=0.0)
@@ -38,14 +65,12 @@ class CombinedPlanner(object):
             cur_state = copy.copy(cur_state)
             cur_state.reset_mpc_related()
             state_idx +=1
-            print(f"idx={state_idx}",end=" ")
-            if self.lp.local_obs_detected(cur_state, cone_radius=SENSE_CONE_RADIUS, cone_fov=SENSE_CONE_ANGLE):
+            print(f"idx={state_idx}")
+            if self.local_obs_detected(cur_state, cone_radius=SENSE_CONE_RADIUS, cone_fov=SENSE_CONE_ANGLE):
                 self.mpc_mode = True
                 self.mpc_curr_iteration = 0
 
-            cur_state.v = self.lp.proportional_control_acceleration(TARGETED_SPEED, cur_state.v, DELTA_T)
-
-            if self.mpc_mode:
+            if self.mpc_mode and MPC_ENABLE:
                 delta, _, closest_index = self.mpc.predict(cur_state, DELTA_T)
                 # TODO: once finalized MPC w/ KRRT imp inside, we will replace the PP w/ it.
                 # for now, we just want to statistics and plots over what it does
@@ -62,6 +87,16 @@ class CombinedPlanner(object):
             else:
                 delta, target_ind, closest_index = self.lp.pure_pursuit_steer_control(cur_state, self.trajectory, DELTA_T)
 
+            if DEBUG_ENABLE: print(f"delta {delta}", end=" ")
+            target_speed = TARGETED_SPEED
+            target_speed_coef = 0.01
+            target_speed_decrease_gain = 10
+            target_speed_delta_diff = np.abs(delta) - target_speed_coef * MAX_DSTEER
+            if  target_speed_delta_diff > 0:
+                target_speed = max(0,TARGETED_SPEED - target_speed_decrease_gain*target_speed_delta_diff)
+                if DEBUG_ENABLE: print(f"target_speed {target_speed}")
+
+            cur_state.v = self.lp.proportional_control_acceleration(target_speed, cur_state.v, DELTA_T)
             cur_state.predelta = delta
             cur_state = self.sim.update_state(cur_state, delta)  # Control vehicle
             self.clock += DELTA_T
@@ -70,5 +105,5 @@ class CombinedPlanner(object):
             self.states.append(cur_state)
             self.closest_path_coords.append([self.trajectory.cx[closest_index], self.trajectory.cy[closest_index]])
             self.target_path_coords.append([self.trajectory.cx[target_ind], self.trajectory.cy[target_ind]])
-            print("")
+            if DEBUG_ENABLE: print("")
 

model_predictive_controller.py
@@ -85,8 +85,9 @@ class ModelPredictiveController(object):
         # reduce the current theta to get the real world alpha angle
         alpha -=  state.yaw
 
+        L = self.calc_distance(state.rear_x, state.rear_y, next_state[0], next_state[1])
         # use the formula that delta = arctan(2 * WB * sin(alpha) / L)
-        theta = math.atan2(2.0 * WB * math.sin(alpha) , self.lf)
+        theta = math.atan2(2.0 * WB * math.sin(alpha) , L)
 
         max_change_in_theta = MAX_DSTEER * dt
         theta = np.clip(theta, state.predelta - max_change_in_theta, state.predelta + max_change_in_theta)
@@ -103,7 +104,6 @@ class ModelPredictiveController(object):
         state.mpc_local_goal_pixel = self.converter.meter2pixel(local_goal)
         state.mpc_bbox = self.calc_search_area(local_start_pixel, state.mpc_local_goal_pixel)
         x_min, y_min, x_max, y_max= state.mpc_bbox
-        print(f"state.mpc_bbox {state.mpc_bbox}")
         assert(x_max - x_min >= 10)
         assert(y_max - y_min >= 10)
         cropped_area = self.obs_map[y_min:y_max, x_min:x_max]
@@ -130,11 +130,11 @@ class ModelPredictiveController(object):
             min_cost_index = krrt_costs.index(min(krrt_costs))
             min_cost_path = krrt_paths[min_cost_index]
             state.krrt_path = self.convert_local_path_to_global_map(x_min, y_min, min_cost_path)
-            print("local plan found", end=" ")
+            if DEBUG_ENABLE: print("local plan found", end=" ")
             delta = self.steer_control(state, dt)
             return delta, krrt_target_ind, closest_index
         else:
-            print("No local plan found", end=" ")
+            if DEBUG_ENABLE: print("No local plan found", end=" ")
             return None, None, None
 
 